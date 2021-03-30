ef664852aee1e79fb4150e7c61a3a370549083aa
Vitaly1785 <korsar1785@yandex.ru>
2021-03-30 11:25:18 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson_9_rest/Lesson9RestApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson_9_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson9RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson9RestApplication.class, args);
+    }
+
+}
